02c2de4c589aeffe9422439a4d4695e261ad7777
rtoshkov <rosentoshkov@gmail.com>
2023-05-15 14:13:12 +0300

    video recording
    Added option for recording video continuously and option for recording video only when motion is detected

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import {RingApi} from 'ring-client-api'
 import {refreshToken} from './config.js'
-import {readFile,writeFile} from "fs";
+import {readFile, writeFile} from "fs";
 import {promisify} from "util";
-
-
 
 
 const ringApi = new RingApi({
@@ -12,7 +10,7 @@ const ringApi = new RingApi({
 })
 
 ringApi.onRefreshTokenUpdated.subscribe(
-    async ({ newRefreshToken, oldRefreshToken }) => {
+    async ({newRefreshToken, oldRefreshToken}) => {
         console.log('Refresh Token Updated: ', newRefreshToken)
         if (!oldRefreshToken) {
             return
@@ -27,10 +25,33 @@ ringApi.onRefreshTokenUpdated.subscribe(
     }
 )
 
-
 const locations = await ringApi.getLocations()
 const myCamera = locations[0].cameras[0]
-myCamera.getSnapshot()
 
 
+async function recordVideo(sec=600, record=false){
+    do{
+        const currentDate = new Date();
+        const fileName = 'Ring-'
+            + currentDate.getFullYear()
+            + '-' + (currentDate.getUTCMonth() + 1)
+            + '-' + currentDate.getDate()
+            + '-' + currentDate.getHours() + '_'
+            + currentDate.getMinutes() + '.mp4';
+
+        const result = await myCamera.recordToFile(fileName, sec)
+        console.log('RECORDER: ' + fileName)
+    }
+    while(record);
+}
+
+
+// Да записва при нотификация за движение
+myCamera.onMotionDetected.subscribe((data) => {
+    recordVideo(120) // Цифрата е колко секунди да записва
+})
+
+
+// Разкоментирай долният ред ако искаш да включиш непрекъснат запис и !! Закоментирай горните три реда за да махнеш записа при движение.
+// recordVideo(60, true)
 
